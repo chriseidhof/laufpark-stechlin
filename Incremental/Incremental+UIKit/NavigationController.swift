a2a3b1e02f9aa4cd2a1838cc79cbf96f93a8a2c5
Chris Eidhof <chris@eidhof.nl>
2017-10-02 18:32:05 +0200

    Navigation controller

---
+++ Incremental/Incremental+UIKit/NavigationController.swift
@@ -0,0 +1,52 @@
+//
+//  NavigationController.swift
+//  Incremental
+//
+//  Created by Chris Eidhof on 02.10.17.
+//  Copyright © 2017 objc.io. All rights reserved.
+//
+
+import Foundation
+
+func navigationController(_ viewControllers: ArrayWithHistory<IBox<UIViewController>>) -> IBox<UINavigationController> {
+    let nc = UINavigationController()
+    let result = IBox(nc)
+    result.bindViewControllers(to: viewControllers)
+    return result
+}
+
+extension IBox where V: UINavigationController {
+    func appendViewController(_ vc: IBox<UIViewController>) {
+        self.unbox.viewControllers.append(vc.unbox)
+        self.disposables.append(vc)
+    }
+    
+    public func bindViewControllers(to value: ArrayWithHistory<IBox<UIViewController>>) {
+        self.disposables.append(value.observe(current: { initialVCs in
+            assert(self.unbox.viewControllers == [])
+            for v in initialVCs {
+                self.appendViewController(v)
+            }
+        }) { [unowned self] in
+            switch $0 {
+            case let .insert(v, at: i):
+                if i == self.unbox.viewControllers.count {
+                    self.unbox.pushViewController(v.unbox, animated: true)
+                } else {
+                    self.unbox.viewControllers.insert(v.unbox, at: i)
+                }
+                self.disposables.append(v)
+            case .remove(at: let i):
+                let v: UIViewController = self.unbox.viewControllers[i]
+                let index = self.disposables.index { d in
+                    if let vcBox = d as? IBox<UIViewController>, vcBox.unbox === v {
+                        return true
+                    }
+                    return false
+                }
+                self.disposables.remove(at: index!)
+                self.unbox.viewControllers.remove(at: i)
+            }
+        })
+    }
+}
