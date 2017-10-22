14d49ebac3e1089bd52f85b3d86484a60aac5b85
Chris Eidhof <chris@eidhof.nl>
2017-10-22 15:25:16 +0200

    Nicer formatting for the elevation profile

--- Laufpark/LineView.swift
+++ Laufpark/LineView.swift
@@ -9,6 +9,15 @@
 import UIKit
 import MapKit
 
+extension CGContext {
+    func drawLine(from start: CGPoint, to end: CGPoint, color: UIColor) {
+        color.setStroke()
+        move(to: start)
+        addLine(to: end)
+        strokePath()
+    }
+}
+
 final class LineView: UIView {
     struct Point: Equatable {
         static func ==(lhs: LineView.Point, rhs: LineView.Point) -> Bool {
@@ -38,8 +47,9 @@ final class LineView: UIView {
         }
     }
     
-    var horizontalTick: CGFloat? {
-        return distanceFormatter.units == .metric ? 5000 : 4828.03
+    var horizontalTick: CGFloat {
+        // distanceFormatter.units doesn't return the right value...
+        return distanceFormatter.locale.usesMetricSystem ? 5000 : 4828.03
     }
     var tickColor: UIColor = UIColor.gray.withAlphaComponent(0.3) { didSet { setNeedsDisplay() } }
     
@@ -54,7 +64,7 @@ final class LineView: UIView {
     }
     
     func recomputePointsRect() {
-        var (minY, maxY, maxX): (CGFloat, CGFloat, CGFloat) = (100000, 0, 0)
+        var (minY, maxY, maxX): (CGFloat, CGFloat, CGFloat) = (.greatestFiniteMagnitude, 0, 0)
         for p in points {
             minY = min(minY, CGFloat(p.y))
             maxY = max(maxY, CGFloat(p.y))
@@ -68,43 +78,55 @@ final class LineView: UIView {
         guard !self.points.isEmpty else { return }
         guard let context = UIGraphicsGetCurrentContext() else { return }
 
-        context.translateBy(x: 0, y: bounds.size.height)
+
+        let labelPadding: CGFloat = 20
         
         let scaleX = bounds.size.width/_pointsRect.size.width
-        let scaleY = bounds.size.height/_pointsRect.size.height
+        let scaleY = (bounds.size.height-labelPadding)/_pointsRect.size.height
+
+
+
+        // drawing ticks
+        let cgTickWidth = horizontalTick * scaleX
+        let ticks: [CGFloat] = Array(sequence(state: cgTickWidth, next: { (currentTick: inout CGFloat) in
+            defer { currentTick += cgTickWidth }
+            guard currentTick < self.bounds.size.width else { return nil }
+            return currentTick
+        }))
+
+        let attributes: [NSAttributedStringKey : Any] = [
+            .foregroundColor: strokeColor,
+            .font: UIFont.systemFont(ofSize: 12)
+        ]
         
-        if let tickWidth = horizontalTick {
-            let cgTickWidth = tickWidth * scaleX
+        for tick in ticks {
+            let start = CGPoint(x: tick, y: 0)
+            let end = CGPoint(x: tick, y: bounds.size.height-labelPadding)
             context.setLineWidth(1)
-            var currentTick = cgTickWidth
-            while currentTick < bounds.size.width {
-                let start = CGPoint(x: currentTick, y: 0)
-                context.move(to: start)
-                context.addLine(to: CGPoint(x: currentTick, y: -bounds.size.height))
-                tickColor.setStroke()
-                context.strokePath()
-                let text = distanceFormatter.string(fromDistance: CLLocationDistance((currentTick/scaleX).rounded()))
-                (text as NSString).draw(at: CGPoint(x: currentTick + 5, y: -15), withAttributes: [
-                    .foregroundColor: strokeColor,
-                    .font: UIFont.systemFont(ofSize: 12)
-                    ])
-                currentTick += cgTickWidth
-            }
+            context.drawLine(from: start, to: end, color: tickColor)
+
+            let text = distanceFormatter.string(fromDistance: CLLocationDistance((tick/scaleX))) as NSString
+            let width = text.size(withAttributes: attributes).width
+            (text as NSString).draw(at: CGPoint(x: tick - (width/2), y: bounds.size.height-labelPadding + 5), withAttributes: attributes)
         }
         
+        // drawing the "cursor"
         if let position = position {
             let start = CGPoint(x: position*scaleX, y: 0)
-            let end = CGPoint(x: position*scaleX, y: -bounds.size.height)
+            let end = CGPoint(x: position*scaleX, y: bounds.size.height-labelPadding)
             context.move(to: start)
             context.addLine(to: end)
             positionColor.setStroke()
             context.strokePath()
         }
         
+        context.saveGState()
+        context.translateBy(x: 0, y: bounds.size.height-labelPadding)
+        context.scaleBy(x: 1, y: -1)
         context.setLineWidth(strokeWidth)
         strokeColor.setStroke()
         let points = self.points.map {
-            CGPoint(x: (CGFloat($0.x)-_pointsRect.origin.x) * scaleX, y: (CGFloat($0.y)-_pointsRect.origin.y) * -scaleY)
+            CGPoint(x: (CGFloat($0.x)-_pointsRect.origin.x) * scaleX, y: (CGFloat($0.y)-_pointsRect.origin.y) * scaleY)
         }
         guard let start = points.first else { return }
         context.move(to: start)
@@ -112,5 +134,6 @@ final class LineView: UIView {
             context.addLine(to: p)
         }
         context.strokePath()
+        context.restoreGState()
     }
 }
