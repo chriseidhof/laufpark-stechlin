896e9a7210f08b59672c257509c5d62603d92a20
Chris Eidhof <chris@eidhof.nl>
2017-11-12 09:34:45 +0100

    Cleanup

--- Laufpark/AppDelegate.swift
+++ Laufpark/AppDelegate.swift
@@ -1,29 +1,5 @@
 import UIKit
 
-
-extension Color {
-    var name: String {
-        switch self {
-        case .red: return "rot"
-        case .turquoise: return "tuerkis"
-        case .brightGreen: return "hellgruen"
-        case .beige: return "beige"
-        case .green: return "gruen"
-        case .purple: return "lila"
-        case .violet: return "violett"
-        case .blue: return "blau"
-        case .brown: return "braun"
-        case .yellow: return "gelb"
-        case .gray: return "grau"
-        case .lightBlue: return "hellblau"
-        case .lightBrown: return "hellbraun"
-        case .orange: return "orange"
-        case .pink: return "pink"
-        case .lightPink: return "rosa"
-        }
-    }
-}
-
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
@@ -36,45 +12,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         window?.rootViewController = mapViewController
         window?.makeKeyAndVisible()
         DispatchQueue(label: "Track Loading").async {
-            let tracks = self.loadTracks()
+            let tracks = Track.load()
             DispatchQueue.main.async {
                 self.mapViewController?.setTracks(tracks)
             }
         }
-        application.applicationSupportsShakeToEdit = true
         return true
     }
     
-    func loadTracks() -> [Track] {
-        let definitions: [(Color, Int)] = [
-            (.red, 4),
-            (.turquoise, 5),
-            (.brightGreen, 7),
-            (.beige, 2),
-            (.green, 4),
-            (.purple, 3),
-            (.violet, 4),
-            (.blue, 3),
-            (.brown, 4),
-            (.yellow, 4),
-            (.gray, 0),
-            (.lightBlue, 4),
-            (.lightBrown, 5),
-            (.orange, 0),
-            (.pink, 4),
-            (.lightPink, 6)
-        ]
-        var allTracks: [[Track]] = []
-        time {
-            allTracks = definitions.map { (color, count) in
-                let trackNames: [(Int, String)] = (0...count).map { ($0, "wabe \(color.name)-strecke \($0)") }
-                return trackNames.map { numberAndName -> Track in
-                    let reader = TrackReader(url: Bundle.main.url(forResource: numberAndName.1, withExtension: "gpx")!)!
-                    return Track(color: color, number: numberAndName.0, name: reader.name, points: reader.points)
-                }
-            }
-        }
-        return Array(allTracks.joined())
-    }
+
 }
 
--- Laufpark/Model.swift
+++ Laufpark/Model.swift
@@ -9,6 +9,28 @@
 import Foundation
 import CoreLocation
 
+extension Color {
+    var name: String {
+        switch self {
+        case .red: return "rot"
+        case .turquoise: return "tuerkis"
+        case .brightGreen: return "hellgruen"
+        case .beige: return "beige"
+        case .green: return "gruen"
+        case .purple: return "lila"
+        case .violet: return "violett"
+        case .blue: return "blau"
+        case .brown: return "braun"
+        case .yellow: return "gelb"
+        case .gray: return "grau"
+        case .lightBlue: return "hellblau"
+        case .lightBrown: return "hellbraun"
+        case .orange: return "orange"
+        case .pink: return "pink"
+        case .lightPink: return "rosa"
+        }
+    }
+}
 
 struct POI {
     let location: CLLocationCoordinate2D
@@ -158,6 +180,38 @@ struct Point {
 extension String {
     func remove(prefix: String) -> String {
         return String(dropFirst(prefix.count))
+    }
+}
+
+extension Track {
+    static func load() -> [Track] {
+        let definitions: [(Color, Int)] = [
+            (.red, 4),
+            (.turquoise, 5),
+            (.brightGreen, 7),
+            (.beige, 2),
+            (.green, 4),
+            (.purple, 3),
+            (.violet, 4),
+            (.blue, 3),
+            (.brown, 4),
+            (.yellow, 4),
+            (.gray, 0),
+            (.lightBlue, 4),
+            (.lightBrown, 5),
+            (.orange, 0),
+            (.pink, 4),
+            (.lightPink, 6)
+        ]
+        var allTracks: [[Track]] = []
+        allTracks = definitions.map { (color, count) in
+            let trackNames: [(Int, String)] = (0...count).map { ($0, "wabe \(color.name)-strecke \($0)") }
+            return trackNames.map { numberAndName -> Track in
+                let reader = TrackReader(url: Bundle.main.url(forResource: numberAndName.1, withExtension: "gpx")!)!
+                return Track(color: color, number: numberAndName.0, name: reader.name, points: reader.points)
+            }
+        }
+        return Array(allTracks.joined())
     }
 }
 
